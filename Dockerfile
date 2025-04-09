2f46d5c3d8c054d319c46e077136198a27f766da
WeddedMercury15 <96825350+WeddedMercury15@users.noreply.github.com>
2025-04-09 23:42:50 +0800

    Update

--- Dockerfile
+++ Dockerfile
@@ -17,4 +17,11 @@ COPY start.sh ./
 RUN chmod +x start.sh
 
 # Define the default command to run the application
-ENTRYPOINT ["./start.sh"]
+ENTRYPOINT ["sh", "-c", \
+    "VENV_DIR=\"$PWD/venv\" && " \
+    "if [ ! -d \"$VENV_DIR\" ]; then python -m venv \"$VENV_DIR\"; fi && " \
+    "source \"$VENV_DIR/bin/activate\" && " \
+    "python -m pip install --no-cache-dir --upgrade pip && " \
+    "python -m pip install --no-cache-dir endstone && " \
+    "exec ./start.sh" \
+]
--- start.sh
+++ start.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-# Define the virtual environment directory
-VENV_DIR="$PWD/venv"
-
-# Create the virtual environment if it doesn't exist
-if [ ! -d "$VENV_DIR" ]; then
-    python -m venv "$VENV_DIR"
-fi
-
-# Activate the virtual environment
-source "$VENV_DIR/bin/activate"
-
 # Configure pip mirror as default source for this venv
 pip config set --site global.index-url https://repo.huaweicloud.com/repository/pypi/simple/
 pip config set --site global.trusted-host repo.huaweicloud.com
-
-# Upgrade pip to the latest version
-python -m pip install --no-cache-dir --upgrade pip
-
-# Install the 'endstone' package
-python -m pip install --no-cache-dir endstone
 
 # Run 'endstone'
 python -m endstone -s .
