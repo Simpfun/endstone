c6f9c2a5c25f891f5720f165fd1e355d8e1eb419
WeddedMercury15 <96825350+WeddedMercury15@users.noreply.github.com>
2025-04-11 22:24:43 +0800

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,41 +4,52 @@
 VENV_DIR="$PWD/venv"
 
 # Create the virtual environment if it doesn't exist
-echo "正在检查虚拟环境..."
+printf "正在检查虚拟环境..."
+printf "\n"
 if [ ! -d "$VENV_DIR" ]; then
     sleep 1
-    echo "未找到虚拟环境，正在初始化..."
+    printf "未找到虚拟环境，正在初始化..."
     python -m venv "$VENV_DIR"
-    echo "虚拟环境初始化完毕。"
+    printf "虚拟环境初始化完毕。"
 else
     sleep 1
-    echo "虚拟环境检查完毕。"
+    printf "虚拟环境检查完毕。"
 fi
+
+printf "\n"
 
 sleep 1
 
 # Activate the virtual environment
-echo -e "\n正在激活虚拟环境..."
+printf "正在激活虚拟环境..."
 source "$VENV_DIR/bin/activate"
+
+printf "\n"
 
 sleep 1
 
 # Upgrade pip to the latest version
-echo -e "\n"
 python -m pip install --no-cache-dir -i https://repo.huaweicloud.com/repository/pypi/simple --trusted-host repo.huaweicloud.com --upgrade pip
 
+printf "\n"
+
 # Check if start.sh exists in target directory
-echo -e "\n正在检查启动脚本..."
+printf "正在检查启动脚本..."
 if [ ! -f "/home/container/start.sh" ]; then
     cp /usr/local/bin/start.sh /home/container/start.sh
 fi
-sleep 1
-echo -e "\n启动脚本检查完毕..."
 
 # Ensure the start script is executable
 chmod +x /home/container/start.sh
 
+sleep 1
+printf "启动脚本检查完毕..."
+
+sleep 1
+
+printf "\n"
+
 # Launch the application script
-echo -e "\n正在执行启动脚本..."
-echo -e "\n"
+printf "正在执行启动脚本..."
+printf "\n"
 exec /home/container/start.sh
